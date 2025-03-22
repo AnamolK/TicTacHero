60308bc68ba868d8f77a453d232e2574d69fe8f3
kchen0567 <kechen68145@gmail.com>
2025-03-21 19:58:19 -0700

    Enemy Hitbox Not Working

--- FinalProject/Assets/Scripts/EnemyMovementController.cs
+++ FinalProject/Assets/Scripts/EnemyMovementController.cs
@@ -4,26 +4,84 @@ using UnityEngine;
 
 public class EnemyMovementController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Transform movePoint;
-
     public LayerMask BlockedArea;
 
-    private string mostRecentPress;
+    public float moveTickDuration = 0.5f;
+    public float moveDuration = 0.1f;
+    private Transform player;
+    public bool isMoving = false;
+    public bool canMove = true;
+
 
     void Start()
     {
-        movePoint.parent = null;
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj != null) {
+            player = playerObj.transform;
+        }
+            
+        transform.position = new Vector3(SnapToGrid(transform.position.x), SnapToGrid(transform.position.y), transform.position.z);
+        StartCoroutine(MoveTick());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator MoveTick()
     {
-
-        if (!(Physics2D.OverlapCircle(movePoint.position, 0f, BlockedArea)) || transform.position != movePoint.position) {
-
-            transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-        }        
+        while (true)
+        {
+            yield return new WaitForSeconds(moveTickDuration);
+            if (!isMoving && canMove)
+            {
+                Vector2 direction = GetMoveDirection();
+                if (direction != Vector2.zero)
+                    StartCoroutine(Move(direction));
+            }
+        }
     }
 
+    Vector2 GetMoveDirection()
+    {
+        if (player == null)
+            return Vector2.zero;
+        Vector2 pos = transform.position;
+        Vector2 playerPos = player.position;
+        Vector2 diff = playerPos - pos;
+        if (Mathf.Abs(diff.x) > Mathf.Abs(diff.y))
+            return new Vector2(Mathf.Sign(diff.x), 0);
+        else if (Mathf.Abs(diff.y) > 0)
+            return new Vector2(0, Mathf.Sign(diff.y));
+        return Vector2.zero;
+    }
+
+    IEnumerator Move(Vector2 direction)
+    {
+        isMoving = true;
+        Vector3 startPos = transform.position;
+        Vector3 endPos = startPos + new Vector3(direction.x, direction.y, 0);
+        float elapsed = 0f;
+        while (elapsed < moveDuration)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, elapsed / moveDuration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = new Vector3(SnapToGrid(endPos.x), SnapToGrid(endPos.y), endPos.z);
+        isMoving = false;
+    }
+
+    float SnapToGrid(float value)
+    {
+        return value;
+        //return Mathf.Round(value - 0.5f) + 0.5f;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Hit something: " + collision.gameObject.name);
+        canMove = false;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        canMove = true;
+    }
 }

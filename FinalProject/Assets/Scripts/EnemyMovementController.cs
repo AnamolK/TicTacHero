562f06617ba9df61f3905d3750c31fba80d3040c
kchen0567 <kechen68145@gmail.com>
2025-03-25 22:44:46 -0400

    FIXED HITBOXES ON ENEMIES

--- FinalProject/Assets/Scripts/EnemyMovementController.cs
+++ FinalProject/Assets/Scripts/EnemyMovementController.cs
@@ -4,105 +4,36 @@ using UnityEngine;
 
 public class EnemyMovementController : MonoBehaviour
 {
+    public float moveSpeed = 999f;
+    public Transform movePoint;
+    public Transform playerPosition;
+
     public LayerMask BlockedArea;
 
-    public float moveTickDuration = 0.5f;
-    public float moveDuration = 0.1f;
-    private Transform player;
-    public bool isMoving = false;
-    public bool canMove = true;
-
+    private string mostRecentPress;
 
     void Start()
     {
-        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
-        if (playerObj != null) {
-            player = playerObj.transform;
-        }
-            
-        transform.position = new Vector3(SnapToGrid(transform.position.x), SnapToGrid(transform.position.y), transform.position.z);
-        StartCoroutine(MoveTick());
+        movePoint.parent = null;
     }
 
-    IEnumerator MoveTick()
+    void FixedUpdate()
     {
-        while (true)
-        {
-            yield return new WaitForSeconds(moveTickDuration);
-            
-            if (!isMoving && canMove)
-            {
-                Vector2 direction = GetMoveDirection();
-                if (direction != Vector2.zero)
-                {
-                    Vector3 targetPos = transform.position + new Vector3(direction.x, direction.y, 0);
 
-                    // Check if moving into the player
-                    Collider2D hit = Physics2D.OverlapCircle(targetPos, 0.1f, LayerMask.GetMask("Player"));
-                    if (hit != null)
-                    {
-                        Debug.Log("Enemy attacks the player!");
+        if (movePoint.position.x % 1 == 0 && movePoint.position.y % 1 == 0) {
 
-                        PlayerHealth playerHealth = hit.GetComponent<PlayerHealth>();
-                        if (playerHealth != null)
-                        {
-                            playerHealth.TakeDamage(1); // Customize damage here
-                        }
-                        continue; // Don't move into the player's tile
-                    }
+                var moveTo = new Vector3(movePoint.position.x, movePoint.position.y, 0f);
 
-                    // If not blocked by player, go ahead and move
-                    StartCoroutine(Move(direction));
-                }
+            if (!(playerPosition.position == movePoint.position)) {
+
+                Debug.Log("POSITION: " + moveTo.x + "  , " + moveTo.y);
+                
+                transform.position = Vector3.MoveTowards(transform.position, moveTo, moveSpeed * Time.deltaTime);
+                
+            } else {
+                Debug.Log("COLIDE: " + moveTo.x + "  , " + moveTo.y);  
             }
+
         }
-    }
-
-
-    Vector2 GetMoveDirection()
-    {
-        if (player == null)
-            return Vector2.zero;
-        Vector2 pos = transform.position;
-        Vector2 playerPos = player.position;
-        Vector2 diff = playerPos - pos;
-        if (Mathf.Abs(diff.x) > Mathf.Abs(diff.y))
-            return new Vector2(Mathf.Sign(diff.x), 0);
-        else if (Mathf.Abs(diff.y) > 0)
-            return new Vector2(0, Mathf.Sign(diff.y));
-        return Vector2.zero;
-    }
-
-    IEnumerator Move(Vector2 direction)
-    {
-        isMoving = true;
-        Vector3 startPos = transform.position;
-        Vector3 endPos = startPos + new Vector3(direction.x, direction.y, 0);
-        float elapsed = 0f;
-        while (elapsed < moveDuration)
-        {
-            transform.position = Vector3.Lerp(startPos, endPos, elapsed / moveDuration);
-            elapsed += Time.deltaTime;
-            yield return null;
-        }
-        transform.position = new Vector3(SnapToGrid(endPos.x), SnapToGrid(endPos.y), endPos.z);
-        isMoving = false;
-    }
-
-    float SnapToGrid(float value)
-    {
-        return value;
-        //return Mathf.Round(value - 0.5f) + 0.5f;
-    }
-
-    void OnCollisionStay(Collision collision)
-    {
-        Debug.Log("Hit something: " + collision.gameObject.name);
-        canMove = false;
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        canMove = true;
     }
 }

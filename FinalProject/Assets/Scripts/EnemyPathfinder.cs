f52ae56b080362755d4cd9705234eb99d16b19e8
kchen0567 <kechen68145@gmail.com>
2025-03-30 18:55:22 -0400

    Revert "Adding previous scripts that dissapeared"
    This reverts commit 23c9f214884a2f95da1ff0bc9338799869b362f5.

--- FinalProject/Assets/Scripts/EnemyPathfinder.cs
+++ FinalProject/Assets/Scripts/EnemyPathfinder.cs
@@ -1,4 +1,4 @@
-using System.Collections; 
+using System.Collections;
 using UnityEngine;
 
 public class EnemyPathfinder : MonoBehaviour
@@ -8,16 +8,8 @@ public class EnemyPathfinder : MonoBehaviour
     private Transform player;
     public bool isMoving = false;
 
-    // Health settings
-    public int maxHealth = 3;
-    private int currentHealth;
-
-    // Dynamically updated attack side.
-    public string currentAttackSide = "None";
-
     void Start()
     {
-        currentHealth = maxHealth;
         GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
         if (playerObj != null)
             player = playerObj.transform;
@@ -34,11 +26,7 @@ public class EnemyPathfinder : MonoBehaviour
             {
                 Vector2 direction = GetMoveDirection();
                 if (direction != Vector2.zero)
-                {
-                    // Update the active attack side dynamically based on movement direction.
-                    currentAttackSide = GetDirectionString(direction);
                     StartCoroutine(Move(direction));
-                }
             }
         }
     }
@@ -76,64 +64,6 @@ public class EnemyPathfinder : MonoBehaviour
     float SnapToGrid(float value)
     {
         return value;
-    }
-
-    // Converts movement direction vector to a cardinal direction string.
-    string GetDirectionString(Vector2 direction)
-    {
-        direction.Normalize();
-        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
-            return direction.x > 0 ? "Right" : "Left";
-        else
-            return direction.y > 0 ? "Up" : "Down";
-    }
-
-    public void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-        Debug.Log("Enemy took damage. Current health: " + currentHealth);
-        if (currentHealth <= 0)
-            Die();
-    }
-
-    void Die()
-    {
-        Debug.Log("Enemy died.");
-
-        // Stop all coroutines and disable this script.
-        StopAllCoroutines();
-        this.enabled = false;
-
-
-        Collider2D[] cols = transform.root.GetComponentsInChildren<Collider2D>();
-        foreach (Collider2D col in cols)
-        {
-            col.enabled = false;
-        }
-
-        SpriteRenderer[] srs = transform.root.GetComponentsInChildren<SpriteRenderer>();
-        foreach (SpriteRenderer sr in srs)
-        {
-            sr.enabled = false;
-        }
-
-
-        Transform enemyAsset = transform.root.Find("EnemyAsset");
-        if(enemyAsset != null)
-            enemyAsset.gameObject.SetActive(false);
-
-        Transform enemyAttackside = transform.root.Find("EnemyAttackside");
-        if(enemyAttackside != null)
-            enemyAttackside.gameObject.SetActive(false);
-
-
-        EnemyMovementController moveCtrl = GetComponentInParent<EnemyMovementController>();
-        if (moveCtrl != null)
-            moveCtrl.Die();
-
-
-        transform.root.gameObject.SetActive(false);
-
-        Destroy(transform.root.gameObject, 0.1f);
+        //return Mathf.Round(value - 0.5f) + 0.5f;
     }
 }

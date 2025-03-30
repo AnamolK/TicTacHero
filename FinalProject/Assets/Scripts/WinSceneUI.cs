809d255ca15b88574a6aac71a4f72031cad3bba2
elaabay <ela.abay@tufts.edu>
2025-03-30 14:52:28 -0400

    win scene added

---
+++ FinalProject/Assets/Scripts/WinSceneUI.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinSceneUI : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}

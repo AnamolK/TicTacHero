d399ec74ac5988e8be7728a7c55a30d41442732d
AnamolK <kaspal.anamol@gmail.com>
2025-03-29 22:57:17 -0400

    Fixed error where the enemy only attacked once if you don't move

--- FinalProject/Assets/Scripts/PlayerHealth.cs
+++ FinalProject/Assets/Scripts/PlayerHealth.cs
@@ -1,7 +1,6 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -9,6 +8,12 @@ public class PlayerHealth : MonoBehaviour
     private int currentHealth;
     public TMP_Text healthText;
     public TMP_Text gameOverText;
+
+    // Time in seconds between damage ticks
+    public float damageInterval = 1f;
+
+
+    private Coroutine damageCoroutine;
 
     void Start()
     {
@@ -22,6 +27,7 @@ public class PlayerHealth : MonoBehaviour
     {
         currentHealth -= damage;
         UpdateUI();
+        Debug.Log("Player took damage. Current health: " + currentHealth);
         if (currentHealth <= 0)
             Die();
     }
@@ -48,9 +54,43 @@ public class PlayerHealth : MonoBehaviour
         Time.timeScale = 0f;
     }
 
+    // Start damage coroutine when an enemy enters the trigger
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
+        {
+            // If a coroutine isnt running, start one
+            if (damageCoroutine == null)
+            {
+                damageCoroutine = StartCoroutine(ApplyDamageOverTime());
+            }
+        }
+    }
+
+    // Stop coroutine when the enemy leaves the trigger
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            if (damageCoroutine != null)
+            {
+                StopCoroutine(damageCoroutine);
+                damageCoroutine = null;
+            }
+        }
+    }
+
+    // Coroutine that repeatedly applies damage at fixed intervals
+    IEnumerator ApplyDamageOverTime()
+    {
+        // Apply damage immediately upon entering
+        TakeDamage(1);
+        yield return new WaitForSeconds(damageInterval);
+
+        while (true)
+        {
             TakeDamage(1);
+            yield return new WaitForSeconds(damageInterval);
+        }
     }
 }

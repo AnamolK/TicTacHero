5b5d745501053d95f4f4f7d5cf3fb2d32bd20129
fouadsayegh2 <fouad.sayegh@tufts.edu>
2025-03-31 00:01:06 -0400

    Main Menu

--- FinalProject/Assets/Scripts/GameHandler.cs
+++ FinalProject/Assets/Scripts/GameHandler.cs
@@ -4,13 +4,24 @@ using UnityEngine.SceneManagement;
 public class GameHandler : MonoBehaviour
 {
     public string mainMenuSceneName = "MainMenu";
-    public string gameSceneName = "SampleScene"; // or your real game scene name
+    public string gameSceneName = "SampleScene";
     public string winSceneName = "WinScene";
     public string loseSceneName = "LoseScene";
 
     public void StartGame()
     {
-        SceneManager.LoadScene(gameSceneName);
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
     }
 
     public void RestartGame()
@@ -31,13 +42,5 @@ public class GameHandler : MonoBehaviour
     public void LoseGame()
     {
         SceneManager.LoadScene(loseSceneName);
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-        #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-        #endif
     }
 }
